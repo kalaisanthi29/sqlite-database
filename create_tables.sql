69e218a265260a524ef93239ad79bff3abc1e6b1
kalaisanthi29 <ksanthii29@live.com>
2026-05-19 17:33:34 +0200

    Update create_tables.sql

--- create_tables.sql
+++ create_tables.sql
@@ -1,4 +1,31 @@
+-- table for authors
+
 CREATE TABLE Authors (
     author_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL
+    name TEXT NOT NULL,
+    country TEXT,
+    birth_year INTEGER
+);
+
+
+-- table for books
+
+CREATE TABLE Books (
+    book_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    genre TEXT,
+    author_id INTEGER,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
+
+
+-- table for quotes
+
+CREATE TABLE Quotes (
+    quote_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    quote_text TEXT NOT NULL,
+    book_id INTEGER,
+    author_id INTEGER,
+    FOREIGN KEY (book_id) REFERENCES Books(book_id),
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
